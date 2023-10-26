662187496c282882ec0165e754b4c783556a26a8
Zvi Lorber <zvi@dataloop.ai>
2023-10-26 18:58:01 +0300

    v4

--- modules/main.py
+++ modules/main.py
@@ -15,14 +15,14 @@ class ServiceRunner(dl.BaseServiceRunner):
     @staticmethod
     def one(item: dl.Item) -> dl.Item:
         logging.info(f"Running service one on item: {item.id}")
-        item.metadata['service_one'] = 'service one v2'
+        item.metadata['service_one'] = 'service one v4'
         item.update()
         return item
 
     @staticmethod
     def two(item: dl.Item) -> dl.Item:
         logging.info(f"Running service two on item: {item.id}")
-        item.metadata['service_two'] = 'service two v2'
+        item.metadata['service_two'] = 'service two v4'
         item.update()
         return item
 
